eac9afd1c7ffc67023d134884b3f42b154766765
Vatsal Joshi <vatz88@gmail.com>
2022-09-16 14:57:22 +0530

    no-console-statements babel plugin

--- src/no-console-statements.js
+++ src/no-console-statements.js
@@ -1,5 +1,13 @@
 module.exports = function ({ types: t }) {
   return {
-    visitor: {},
+    visitor: {
+      CallExpression(path) {
+        const callee = path.get('callee');
+        object = callee.get('object');
+        if (object.isIdentifier({ name: 'console' })) {
+          path.remove();
+        }
+      },
+    },
   };
 };
